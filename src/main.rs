8eb484b696a16e8caa5ae01c915a5d36799977aa
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-10-09 02:05:12 -0400

    Parse arguments and handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod parser;
+mod log;
+
+use log::log_fatal;
+use parser::ParseError;
+use std::env;
+
 fn main() {
-    println!("Hello, JDX");
+    let command = parser::parse_arguments(env::args().collect())
+        .unwrap_or_else(|error| log_fatal(
+            match error {
+                ParseError::NoArguments => "No arguments given.".to_string(),
+                ParseError::InvalidCommand(command) => format!("Unrecognized command: '{}'.", command),
+                ParseError::MissingInput => "Missing input path(s). Specify with option '-i'.".to_string(),
+                ParseError::MissingOutput => "Missing output path(s). Specify with option '-o'.".to_string(),
+                ParseError::MissingUnknown(option) => format!("Missing unknown option: '{}'. Please file an issue on the jdx-clt GitHub repository for maintainers to add a proper error.", option),
+            }
+        ));
 }
