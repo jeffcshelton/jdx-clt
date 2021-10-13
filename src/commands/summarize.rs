2cb24922f6c1d1b6251face9872d9cf1c9d65955
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-10-12 21:54:20 -0400

    Change header reading and color type display

--- src/commands/summarize.rs
+++ src/commands/summarize.rs
@@ -1,38 +1,27 @@
-use jdx_rust::jdx::{
-    self,
-    Header,
-    ColorType,
-};
-
-use crate::log::log_error;
+use jdx_rust::jdx::{self, Header};
+use crate::log::log_warning;
 
 pub fn summarize(inputs: Vec<String>) -> jdx::Result<()> {
     for path in inputs {
         println!();
 
-        if !path.ends_with(".jdx") {
-            log_error(format!("File '{}' is not a JDX file.", path));
-            continue;
-        }
-
-        let header = Header::read_from_file(&path);
-        if header.is_err() {
-            log_error(format!("Failed to open file '{}'. File could be corrupted, not exist, or require permissions.", path));
-            continue;
-        }
-
-        let header = header.unwrap();
-        let color_text = match header.color_type {
-            ColorType::Gray => "\x1b[38;5;8;1mGRAY\x1b[0m",
-            ColorType::RGB => "\x1b[1;31mR\x1b[32mG\x1b[34mB\x1b[0m",
-            ColorType::RGBA => "\x1b[1;31R\x1b[32mG\x1b[34mB\x1b[37mA\x1b[0m",
+        let header = Header::read_from_path(&path)?;
+        let color_text = match header.bit_depth {
+            8 => "\x1b[38;5;8;1mGRAY\x1b[0m",
+            24 => "\x1b[1;31mR\x1b[32mG\x1b[34mB\x1b[0m",
+            32 => "\x1b[1;31R\x1b[32mG\x1b[34mB\x1b[37mA\x1b[0m",
+            _ => return Err(jdx::Error::CorruptFile(path)),
         };
 
         println!("=== \x1b[33;1m{}\x1b[0m ===", path);
         println!("JDX File \x1b[34mv{}.{}.{}\x1b[0m", header.version.major, header.version.minor, header.version.patch);
-        println!("Color type: {}", color_text);
+        println!("Color type: {} ({} bits)", color_text, header.bit_depth);
         println!("Image size: \x1b[1m{}\x1b[0m x \x1b[1m{}\x1b[0m", header.image_width, header.image_height);
         println!("Number of images: \x1b[1m{}\x1b[0m", header.item_count);
+
+        if !path.ends_with(".jdx") {
+            log_warning(format!("JDX files should end with extension '.jdx'."));
+        }
     }
 
     println!();
