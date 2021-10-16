06c9ebf9f22b3c8b52460c7d965c87d5e9b43070
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-10-16 00:39:38 -0400

    Add .jdx extension warning

--- src/commands/concatenate.rs
+++ src/commands/concatenate.rs
@@ -1,7 +1,12 @@
 use jdx_rust::jdx::{self, Dataset};
+use crate::log::log_warning;
 
 pub fn concatenate(inputs: Vec<String>, output: String) -> jdx::Result<()> {
     let mut concat_dset = Dataset::read_from_path(&inputs[0])?;
+
+    if !output.ends_with(".jdx") {
+        log_warning("JDX files should end with extension '.jdx'.");
+    }
 
     for i in 1..inputs.len() {
         let ref path = inputs[i];
