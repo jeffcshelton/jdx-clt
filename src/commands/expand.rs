c8339e23cd1e618ae8578a447350c2f3c6bc1ca8
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-04-28 12:43:41 -0400

    Replace all command fns with todo! placeholders
    This is so that they can all be rewritten due to the drastic library changes

--- src/commands/expand.rs
+++ src/commands/expand.rs
@@ -1,61 +1,65 @@
-use image::{GrayImage, ImageFormat, RgbImage, RgbaImage};
-use jdx_rust::jdx::{self, Dataset};
-use std::{path::PathBuf, fs};
-use crate::log::log_fatal;
+// use image::{GrayImage, ImageFormat, RgbImage, RgbaImage};
+// use std::{path::PathBuf, fs};
+// use crate::log::log_fatal;
+// use jdx::Dataset;
+
+// pub fn expand(input: String, output: String) -> jdx::Result<()> {
+// 	let dataset = Dataset::read_from_path(&input)?;
+// 	let output_path = PathBuf::from(&output);
+
+// 	if output_path.exists() {
+// 		log_fatal("Invalid output path: File/directory already exists.");
+// 	}
+
+// 	if fs::create_dir(&output_path).is_err() {
+// 		log_fatal(format!("Failed to create directory at '{}'.", output));
+// 	}
+
+// 	for (i, item) in dataset.items.iter().enumerate() {
+// 		let label_path = output_path.join(item.label.to_string());
+
+// 		if !label_path.exists() && fs::create_dir(&label_path).is_err() {
+// 			log_fatal(format!("Failed to create directory '{}'.", label_path.to_string_lossy()));
+// 		}
+
+// 		let image_path = label_path.join(format!("{}.png", i));
+
+// 		// TODO: Try to optimize and simplify this code
+// 		if dataset.header.bit_depth == 8 {
+// 			let image = match GrayImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
+// 				Some(image) => image,
+// 				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
+// 			};
+
+// 			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
+// 				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
+// 			}
+// 		} else if dataset.header.bit_depth == 24 {
+// 			let image = match RgbImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
+// 				Some(image) => image,
+// 				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
+// 			};
+
+// 			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
+// 				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
+// 			}
+// 		} else if dataset.header.bit_depth == 32 {
+// 			let image = match RgbaImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
+// 				Some(image) => image,
+// 				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
+// 			};
+
+// 			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
+// 				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
+// 			}
+// 		} else {
+// 			log_fatal(format!("Impossible bit depth '{}'. Please file an issue on GitHub for this error.", dataset.header.bit_depth));
+// 		}
+// 	}
+
+// 	Ok(())
+// }
 
 pub fn expand(input: String, output: String) -> jdx::Result<()> {
-	let dataset = Dataset::read_from_path(input)?;
-	let output_path = PathBuf::from(&output);
-
-	if output_path.exists() {
-		log_fatal("Invalid output path: File/directory already exists.");
-	}
-
-	if fs::create_dir(&output_path).is_err() {
-		log_fatal(format!("Failed to create directory at '{}'.", output));
-	}
-
-	for (i, item) in dataset.items.iter().enumerate() {
-		let label_path = output_path.join(item.label.to_string());
-
-		if !label_path.exists() && fs::create_dir(&label_path).is_err() {
-			log_fatal(format!("Failed to create directory '{}'.", label_path.to_string_lossy()));
-		}
-
-		let image_path = label_path.join(format!("{}.png", i));
-
-		// TODO: Try to optimize and simplify this code
-		if dataset.header.bit_depth == 8 {
-			let image = match GrayImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
-				Some(image) => image,
-				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
-			};
-
-			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
-				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
-			}
-		} else if dataset.header.bit_depth == 24 {
-			let image = match RgbImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
-				Some(image) => image,
-				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
-			};
-
-			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
-				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
-			}
-		} else if dataset.header.bit_depth == 32 {
-			let image = match RgbaImage::from_vec(item.width as u32, item.height as u32, item.data.clone()) {
-				Some(image) => image,
-				None => log_fatal(format!("Failed to parse data into image for path '{}'.", image_path.to_string_lossy())),
-			};
-
-			if image.save_with_format(&image_path, ImageFormat::Png).is_err() {
-				log_fatal(format!("Failed to save image to path '{}'.", image_path.to_string_lossy()));
-			}
-		} else {
-			log_fatal(format!("Impossible bit depth '{}'. Please file an issue on GitHub for this error.", dataset.header.bit_depth));
-		}
-	}
-
-	Ok(())
+	todo!()
 }
--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,129 +1,133 @@
-use std::{ffi::OsStr, fs, path::PathBuf};
-use crate::log::{log_fatal, log_warning};
-use jdx_rust::jdx::{self, Dataset};
-use image::{ColorType, GenericImageView, io::Reader as ImageReader};
+// use image::{ColorType, GenericImageView, io::Reader as ImageReader};
+// use std::{ffi::OsStr, fs, path::PathBuf};
+// use crate::log::{log_fatal, log_warning};
+// use jdx::Dataset;
 
-fn string_from_color(color: ColorType) -> &'static str {
-	match color {
-		ColorType::L8 => "L8",
-		ColorType::La8 => "LA8",
-		ColorType::Rgb8 => "RGB8",
-		ColorType::Rgba8 => "RGBA8",
-		ColorType::L16 => "L16",
-		ColorType::La16 => "LA16",
-		ColorType::Rgb16 => "RGB16",
-		ColorType::Rgba16 => "RGBA16",
-		ColorType::Bgr8 => "BGR8",
-		ColorType::Bgra8 => "BGRA8",
-		_ => "UNKNOWN",
-	}
-}
+// fn string_from_color(color: ColorType) -> &'static str {
+// 	match color {
+// 		ColorType::L8 => "L8",
+// 		ColorType::La8 => "LA8",
+// 		ColorType::Rgb8 => "RGB8",
+// 		ColorType::Rgba8 => "RGBA8",
+// 		ColorType::L16 => "L16",
+// 		ColorType::La16 => "LA16",
+// 		ColorType::Rgb16 => "RGB16",
+// 		ColorType::Rgba16 => "RGBA16",
+// 		ColorType::Bgr8 => "BGR8",
+// 		ColorType::Bgra8 => "BGRA8",
+// 		_ => "UNKNOWN",
+// 	}
+// }
 
-// TODO: Consider handling fatal errors with the result instead
-pub fn generate(input: String, output: String) -> jdx::Result<()> {
-	let input_path = PathBuf::from(input);
-	let output_path = PathBuf::from(&output);
+// // TODO: Consider handling fatal errors with the result instead
+// pub fn generate(input: String, output: String) -> jdx::Result<()> {
+// 	let input_path = PathBuf::from(input);
+// 	let output_path = PathBuf::from(&output);
 
-	let category_iterator = fs::read_dir(input_path)
-		.unwrap_or_else(|_| log_fatal("Invalid input path: Must be a valid directory."));
+// 	let category_iterator = fs::read_dir(input_path)
+// 		.unwrap_or_else(|_| log_fatal("Invalid input path: Must be a valid directory."));
    
-	if output_path.exists() {
-		log_fatal("Invalid output path: File already exists.");
-	}
+// 	if output_path.exists() {
+// 		log_fatal("Invalid output path: File already exists.");
+// 	}
 
-	if output_path.extension() != Some(OsStr::new("jdx")) {
-		log_warning("JDX files should end with extension '.jdx'.");
-	}
+// 	if output_path.extension() != Some(OsStr::new("jdx")) {
+// 		log_warning("JDX files should end with extension '.jdx'.");
+// 	}
 
-	let mut dataset = Dataset::default();
-	let mut expected_index: u64 = 0;
+// 	let mut dataset = Dataset::default();
+// 	let mut expected_index: u64 = 0;
 
-	for category in category_iterator {
-		let category_path = category
-			.unwrap_or_else(|_| log_fatal("Invalid input path: Cannot read directory."))
-			.path();
+// 	for category in category_iterator {
+// 		let category_path = category
+// 			.unwrap_or_else(|_| log_fatal("Invalid input path: Cannot read directory."))
+// 			.path();
 
-		let category_string = category_path
-			.file_name()
-			.unwrap_or(OsStr::new("none"))
-			.to_string_lossy();
+// 		let category_string = category_path
+// 			.file_name()
+// 			.unwrap_or(OsStr::new("none"))
+// 			.to_string_lossy();
 		
-		if category_string.starts_with('.') {
-			continue;
-		}
+// 		if category_string.starts_with('.') {
+// 			continue;
+// 		}
 
-		let category_index = match category_string.parse() {
-			Ok(index) => index,
-			Err(_) => log_fatal(format!("Found directory '{}' but expected '{}'. Please ensure directories are labeled consecutively starting with '0'.", category_string, expected_index)),
-		};
+// 		let category_index = match category_string.parse() {
+// 			Ok(index) => index,
+// 			Err(_) => log_fatal(format!("Found directory '{}' but expected '{}'. Please ensure directories are labeled consecutively starting with '0'.", category_string, expected_index)),
+// 		};
 
-		expected_index += 1;
+// 		expected_index += 1;
 
-		let entry_iterator = match fs::read_dir(&category_path) {
-			Ok(iterator) => iterator,
-			Err(_) => {
-				log_warning(format!("Skipping file '{}': Cannot iterate over directory.", category_string));
-				continue;
-			}
-		};
+// 		let entry_iterator = match fs::read_dir(&category_path) {
+// 			Ok(iterator) => iterator,
+// 			Err(_) => {
+// 				log_warning(format!("Skipping file '{}': Cannot iterate over directory.", category_string));
+// 				continue;
+// 			}
+// 		};
 
-		for entry in entry_iterator {
-			let entry_path = entry
-				.unwrap_or_else(|_| log_fatal(format!("Failed to iterate over files in directory '{}'.", category_string)))
-				.path();
+// 		for entry in entry_iterator {
+// 			let entry_path = entry
+// 				.unwrap_or_else(|_| log_fatal(format!("Failed to iterate over files in directory '{}'.", category_string)))
+// 				.path();
 			
-			let entry_string = entry_path
-				.file_name()
-				.unwrap_or(OsStr::new("none"))
-				.to_string_lossy();
+// 			let entry_string = entry_path
+// 				.file_name()
+// 				.unwrap_or(OsStr::new("none"))
+// 				.to_string_lossy();
 
-			if entry_string.starts_with('.') {
-				continue;
-			}
+// 			if entry_string.starts_with('.') {
+// 				continue;
+// 			}
 			
-			let image = ImageReader::open(&entry_path)
-				.unwrap_or_else(|_| log_fatal(format!("Cannot read file '{}' as an image.", entry_string)))
-				.decode()
-				.unwrap_or_else(|_| log_fatal(format!("Cannot decode file '{}' as an image.", entry_string)));
+// 			let image = ImageReader::open(&entry_path)
+// 				.unwrap_or_else(|_| log_fatal(format!("Cannot read file '{}' as an image.", entry_string)))
+// 				.decode()
+// 				.unwrap_or_else(|_| log_fatal(format!("Cannot decode file '{}' as an image.", entry_string)));
 			
-			// TODO: Consider adding size checks for image width and height
-			let width = image.width() as u16;
-			let height = image.height() as u16;
+// 			// TODO: Consider adding size checks for image width and height
+// 			let width = image.width() as u16;
+// 			let height = image.height() as u16;
 
-			let bit_depth = match image.color() {
-				ColorType::L8 => 8,
-				ColorType::Rgb8 => 24,
-				ColorType::Rgba8 => 32,
-				color => log_fatal(format!("Image at '{}' has unsupported color type '{}'.", entry_string, string_from_color(color))),
-			};
+// 			let bit_depth = match image.color() {
+// 				ColorType::L8 => 8,
+// 				ColorType::Rgb8 => 24,
+// 				ColorType::Rgba8 => 32,
+// 				color => log_fatal(format!("Image at '{}' has unsupported color type '{}'.", entry_string, string_from_color(color))),
+// 			};
 			
-			if dataset.header.item_count == 0 {
-				dataset.header.image_width = width;
-				dataset.header.image_height = height;
-				dataset.header.bit_depth = bit_depth;
-			}
+// 			if dataset.header.item_count == 0 {
+// 				dataset.header.image_width = width;
+// 				dataset.header.image_height = height;
+// 				dataset.header.bit_depth = bit_depth;
+// 			}
 
-			if width != dataset.header.image_width {
-				return Err(jdx::Error::UnequalWidths)
-			} else if height != dataset.header.image_height {
-				return Err(jdx::Error::UnequalHeights)
-			} else if bit_depth != dataset.header.bit_depth {
-				return Err(jdx::Error::UnequalBitDepths)
-			}
+// 			if width != dataset.header.image_width {
+// 				return Err(jdx::Error::UnequalWidths)
+// 			} else if height != dataset.header.image_height {
+// 				return Err(jdx::Error::UnequalHeights)
+// 			} else if bit_depth != dataset.header.bit_depth {
+// 				return Err(jdx::Error::UnequalBitDepths)
+// 			}
 
-			dataset.items.push(jdx::Item {
-				data: image.as_bytes().to_vec(),
-				width: width,
-				height: height,
-				bit_depth: bit_depth,
-				label: category_index,
-			});
-		}
-	}
+// 			dataset.items.push(jdx::Image {
+// 				data: image.as_bytes().to_vec(),
+// 				width: width,
+// 				height: height,
+// 				bit_depth: bit_depth,
+// 				label: category_index,
+// 			});
+// 		}
+// 	}
 
-	dataset.header.item_count = dataset.items.len() as u64;
-	dataset.header.version = jdx::Version::current();
-	dataset.write_to_path(output)?;
+// 	dataset.header.item_count = dataset.items.len() as u64;
+// 	dataset.header.version = jdx::Version::current();
+// 	dataset.write_to_path(output)?;
 
-	Ok(())
+// 	Ok(())
+// }
+
+pub fn generate(input: String, output: String) -> jdx::Result<()> {
+	todo!()
 }
