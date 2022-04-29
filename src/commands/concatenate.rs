7aafdbcc0c2db5458d2830b73ed398a1c0ae8869
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-04-29 14:28:23 -0400

    Rewrite concatenate for library update

--- src/commands/concatenate.rs
+++ src/commands/concatenate.rs
@@ -1,26 +1,21 @@
-// use jdx::{self, Dataset};
-// use crate::log::log_warning;
-
-// pub fn concatenate(inputs: Vec<String>, output: String) -> jdx::Result<()> {
-// 	let mut concat_dset = Dataset::read_from_path(&inputs[0])?;
-
-// 	if !output.ends_with(".jdx") {
-// 		log_warning("JDX files should end with extension '.jdx'.");
-// 	}
-
-// 	for i in 1..inputs.len() {
-// 		let ref path = inputs[i];
-// 		let input_dset = Dataset::read_from_path(path)?;
-
-// 		concat_dset.append(&input_dset)?;
-// 	}
-
-// 	concat_dset.header.version = jdx::Version::current();
-// 	concat_dset.write_to_path(output)?;
-
-// 	Ok(())
-// }
+use crate::log_warning;
+use jdx::Dataset;
 
 pub fn concatenate(inputs: Vec<String>, output: String) -> jdx::Result<()> {
-	todo!()
+	if !output.ends_with(".jdx") {
+		log_warning("JDX files should end with extension '.jdx'.");
+	}
+
+	let mut base_dataset = Dataset::read_from_path(inputs.get(0).unwrap())?;
+
+	for path in &inputs[1..] {
+		let dataset = Dataset::read_from_path(path)?;
+		base_dataset.append(dataset)?;
+	}
+
+	// TODO: Add update to Dataset header version
+
+	base_dataset.write_to_path(output)?;
+
+	Ok(())
 }
