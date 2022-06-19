4f8111c2ff23141bb7589e5c19fa9247117b4ccc
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-18 20:00:33 -0400

    Remove explicit option specification requirement from parser

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,18 @@ mod commands;
 mod parser;
 mod log;
 
+use parser::Command;
 use log::*;
 
 fn main() {
-	use parser::Command;
-	use std::env;
-
-	let command = parser::parse_arguments(env::args().collect())
+	let command = parser::parse_arguments()
 		.unwrap_or_else(|error| log_fatal(error));
 
 	match command {
-		Command::Generate { input, output } => commands::generate(input, output),
-		Command::Concatenate { inputs, output } => commands::concatenate(inputs, output),
-		Command::Expand { input, output } => commands::expand(input, output),
-		Command::Summarize { inputs } => commands::summarize(inputs),
+		Command::Generate { input_path, output_path } => commands::generate(input_path, output_path),
+		Command::Concatenate { input_paths, output_path } => commands::concatenate(input_paths, output_path),
+		Command::Expand { input_path, output_path } => commands::expand(input_path, output_path),
+		Command::Summarize { input_paths } => commands::summarize(input_paths),
 		Command::Version => Ok(commands::version()),
 		Command::Help => Ok(commands::help()),
 	}.unwrap_or_else(|error| log_fatal(error));
--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,10 @@
-use std::{fmt, error::Error};
-use crate::log_fatal;
+use std::{fmt, error::Error, env, path::PathBuf};
 
 pub enum Command {
-	Generate { input: String, output: String },
-	Concatenate { inputs: Vec<String>, output: String },
-	Expand { input: String, output: String },
-	Summarize { inputs: Vec<String> },
+	Generate { input_path: PathBuf, output_path: PathBuf },
+	Concatenate { input_paths: Vec<PathBuf>, output_path: PathBuf },
+	Expand { input_path: PathBuf, output_path: PathBuf },
+	Summarize { input_paths: Vec<PathBuf> },
 	Version,
 	Help,
 }
@@ -13,111 +12,112 @@ pub enum Command {
 #[derive(Debug)]
 pub enum ParseError {
 	NoArguments,
-	InvalidCommand(String),
-	NoParameters(String),
+	TooManyArguments,
+
+	UnrecognizedOption(String),
+	UnrecognizedCommand(String),
 
 	MissingInput,
 	MissingOutput,
-	MissingUnknown(String),
-}
-
-impl ParseError {
-	fn missing_option(option: &str) -> Self {
-		match option {
-			"-i" => Self::MissingInput,
-			"-o" => Self::MissingOutput,
-			unknown => Self::MissingUnknown(unknown.to_string()),
-		}
-	}
 }
 
 impl fmt::Display for ParseError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
 			Self::NoArguments => write!(f, "No arguments given."),
-			Self::InvalidCommand(command) => write!(f, "Unrecognized command: '{}'.", command),
-			Self::NoParameters(option) => write!(f, "No parameters given for option '{}'.", option),
+			Self::TooManyArguments => write!(f, "Too many arguments."),
+			Self::UnrecognizedOption(option) => write!(f, "Unrecognized option: '{option}'."),
+			Self::UnrecognizedCommand(command) => write!(f, "Unrecognized command: '{command}'."),
 			Self::MissingInput => write!(f, "Missing input path(s). Specify with option '-i'."),
 			Self::MissingOutput => write!(f, "Missing output path(s). Specify with option '-o'."),
-			Self::MissingUnknown(option) => write!(f, "Missing unknown option: '{}'. Please file an issue on the jdx-clt GitHub repository for maintainers to add a proper error.", option),
 		}
 	}
 }
 
 impl Error for ParseError {}
 
-fn get_params(args: &Vec<String>, option: &str) -> Result<Vec<String>, ParseError> {
-	for a in 2..args.len() {
-		if args[a] == option {
-			let param_start = a + 1;
-			let mut param_end = param_start;
+pub fn parse_arguments() -> Result<Command, ParseError> {
+	let args = &mut env::args();
+	_ = args.next();
 
-			while param_end < args.len() && !args[param_end].starts_with('-') {
-				param_end += 1;
-			}
+	if let Some(action) = args.next() {
+		let command = match action.as_str() {
+			"generate" | "gen" => {
+				let input_path = args.next()
+					.map(|path| PathBuf::from(path))
+					.ok_or(ParseError::MissingInput)?;
+				
+				let output_path = args.next()
+					.map(|path| PathBuf::from(path))
+					.ok_or(ParseError::MissingOutput)?;
 
-			// Option was specified but no parameters were supplied
-			if param_end == param_start {
-				return Err(ParseError::NoParameters(option.to_string()))
-			}
+				Command::Generate {
+					input_path: input_path,
+					output_path: output_path,
+				}
+			},
+			"concatenate" | "concat" => {
+				let mut input_paths = Vec::new();
 
-			// Returns a vector of parameters whose len() must be > 0
-			return Ok(args[param_start..param_end].to_vec())
+				while let Some(arg) = args.next() {
+					if arg.starts_with('-') {
+						if arg == "-o" {
+							break;
+						} else {
+							return Err(ParseError::UnrecognizedOption(arg));
+						}
+					}
+
+					input_paths.push(PathBuf::from(arg));
+				}
+
+				if input_paths.is_empty() {
+					return Err(ParseError::MissingInput);
+				}
+
+				let output_path = args.next()
+					.map(|path| PathBuf::from(path))
+					.ok_or(ParseError::MissingOutput)?;
+	
+				Command::Concatenate {
+					input_paths: input_paths,
+					output_path: output_path,
+				}
+			},
+			"expand" | "exp" => {
+				let input_path = args.next()
+					.map(|path| PathBuf::from(path))
+					.ok_or(ParseError::MissingInput)?;
+				
+				let output_path = args.next()
+					.map(|path| PathBuf::from(path))
+					.ok_or(ParseError::MissingOutput)?;
+
+				Command::Expand {
+					input_path: input_path,
+					output_path: output_path,
+				}
+			},
+			"summarize" | "sum" => {
+				let input_paths = args
+					.map(|arg| PathBuf::from(arg))
+					.collect();
+						
+				Command::Summarize {
+					input_paths: input_paths
+				}
+			},
+			"version" | "ver" => Command::Version,
+			"help" => Command::Help,
+			unknown => return Err(ParseError::UnrecognizedCommand(unknown.to_string())),
+		};
+
+		if args.next().is_some() {
+			return Err(ParseError::TooManyArguments);
 		}
+
+		return Ok(command);
+	} else {
+		return Err(ParseError::NoArguments);
 	}
-
-	Err(ParseError::missing_option(option))
-}
-
-pub fn parse_arguments(args: Vec<String>) -> Result<Command, ParseError> {
-	if args.len() < 2 {
-		return Err(ParseError::NoArguments)
-	}
-
-	Ok(match args[1].as_str() {
-		"generate" | "gen" => {
-			let inputs = get_params(&args, "-i")?;
-			let outputs = get_params(&args, "-o")?;
-
-			if inputs.len() != 1 { log_fatal("Must specify only one input path."); }
-			if outputs.len() != 1 { log_fatal("Must specify only one output path."); }
-
-			Command::Generate {
-				input: inputs[0].clone(),
-				output: outputs[0].clone(),
-			}
-		},
-		"concatenate" | "concat" => {
-			let inputs = get_params(&args, "-i")?;
-			let outputs = get_params(&args, "-o")?;
-
-			if inputs.len() < 2 { log_fatal("Must specify at least two input paths."); }
-			if outputs.len() != 1 { log_fatal("Must specify only one output path."); }
-
-			Command::Concatenate {
-				inputs: inputs,
-				output: outputs[0].clone(),
-			}
-		},
-		"expand" | "exp" => {
-			let inputs = get_params(&args, "-i")?;
-			let outputs = get_params(&args, "-o")?;
-
-			if inputs.len() != 1 { log_fatal("Must specify only one input path."); }
-			if outputs.len() != 1 { log_fatal("Must specify only one output path."); }
-
-			Command::Expand {
-				input: inputs[0].clone(),
-				output: outputs[0].clone(),
-			}
-		},
-		"summarize" | "sum" => {
-			Command::Summarize {
-				inputs: get_params(&args, "-i")?
-			}
-		},
-		"version" | "ver" => Command::Version,
-		"help" => Command::Help,
-		unknown => return Err(ParseError::InvalidCommand(unknown.to_string())),
-	})
 }
