e44bf6c888bb3540c81968d2e1b7718f7ac08228
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-06-19 22:14:42 -0400

    Move help & version strings to main

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,11 +1,9 @@
 mod concatenate;
 mod expand;
 mod generate;
-mod info;
 mod summarize;
 
 pub use concatenate::concatenate;
 pub use expand::expand;
 pub use generate::generate;
-pub use info::{help, version};
 pub use summarize::summarize;
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,28 @@ mod log;
 use parser::Command;
 use log::*;
 
+const HELP_STR: &'static str = "
+Usage:
+  jdx <command> [options...]
+
+Commands:
+  generate (gen)          Generate a new JDX file from a directory of images.
+  concatenate (concat)    Concatenate two or more JDX files into one.
+  expand (exp)            Expand a JDX file into a directory of labeled PNGs.
+  summarize (sum)         Summarize the data in a JDX file.
+  version (ver)           Show the current version information for the JDX CLT.
+  help                    Show this help page.
+
+Options:
+  -o <filename(s)...>     Specifies output file path(s).
+";
+
+const VERSION_STR: &'static str = concat!(
+"\n\x1b[33;1m(JDX)\x1b[39m Jeffrey's Dataset Indexing Tool \x1b[34mv",
+env!("CARGO_PKG_VERSION"),
+"\x1b[0m\nCopyright \x1b[32;1mJeffrey C. Shelton\x1b[0m 2021 (MIT License)\n"
+);
+
 fn main() {
 	let command = parser::parse_arguments()
 		.unwrap_or_else(|error| log_fatal(error));
@@ -14,7 +36,13 @@ fn main() {
 		Command::Concatenate { input_paths, output_path } => commands::concatenate(input_paths, output_path),
 		Command::Expand { input_path, output_path } => commands::expand(input_path, output_path),
 		Command::Summarize { input_paths } => commands::summarize(input_paths),
-		Command::Version => Ok(commands::version()),
-		Command::Help => Ok(commands::help()),
+		Command::Version => {
+			println!("{}", VERSION_STR);
+			Ok(())
+		},
+		Command::Help => {
+			println!("{}", HELP_STR);
+			Ok(())
+		},
 	}.unwrap_or_else(|error| log_fatal(error));
 }
